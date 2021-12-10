6bca32b51c31799546568dde3dc552223f0417c2
Wollffus <58875783+Wollffus@users.noreply.github.com>
2021-12-09 16:17:32 -0800

    Update: Version

--- Wolfhouse_RP_Essentials/fxmanifest.lua
+++ Wolfhouse_RP_Essentials/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 games { 'rdr3', 'gta5' }
 
 author 'https://discord.gg/zGVDppYxfk'
-version '1.4.1'
+version '1.5.1'
 
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
